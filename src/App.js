c7085a306b04ada5b1955a3fa04170c826c928ce
Alexander Makarof <makarofalex@gmail.com>
2020-02-17 17:34:43 +0300

    added main logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+export default class App extends Component {
+  state = {
+       squares:[...Array(8).keys()],
+       count: 0
+     
+  }
+  handleClick = e => {
+    let data = e.target.getAttribute('data-index');
+    this.state.squares[data] = (this.state.count % 2 === 0 ) ? 'X' : 'O';
+    this.setState({
+      squares: this.state.squares
+    })
+    this.setState({
+      count: this.state.count + 1
+    })
+    
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   divStyle = {
+    maxWidth: '300px',
+    display: 'flex',
+    flexWrap: 'wrap'
+  };
+
+  itemStyle = {
+    minWidth: '84px',
+    minHeight: '84px',
+    margin: '1px',
+    border: '1px solid black',
+    display: 'flex',
+    placeContent: 'center',
+  }
+  
+  render() {
+    const {squares} = this.state
+    return (
+      <div 
+      style={
+        this.divStyle
+  }
+      >
+        {squares.map((square, index)=> {
+          return (
+            <div key={index} 
+              style={this.itemStyle}
+              onClick={this.handleClick}
+              data-index={index}
+              className=''>
+              {squares[index]}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
-
-export default App;
